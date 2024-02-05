56aea7066fd22f607f1b75ef4714415b953492af
dmMaksymenko <134098906+dmMaksymenko@users.noreply.github.com>
2024-02-05 15:31:37 +0000

    Deploying to gh-pages from @ dmMaksymenko/goit-js-hw-11@165f3f6e80c8755dffb462e4334f11521c862c7e 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,13 @@
+import{S as u,i as p}from"./assets/vendor-7659544d.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const n of e.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function a(t){if(t.ep)return;t.ep=!0;const e=o(t);fetch(t.href,e)}})();const m=document.querySelector(".search-form"),c=document.querySelector(".gallery"),l="gallery-link";let i;m.addEventListener("submit",function(r){r.preventDefault();const s=r.target.elements.query.value.trim();console.log(s),s!==""&&(g(s).then(o=>{console.log(o),d(o.hits),i=new u(`.${l}`,{captionsData:"alt",captionDelay:250}),i.refresh(),i.on("show.simplelightbox")}),c.innerHTML="",r.target.elements.query.value="")});function g(r){const s="https://pixabay.com/api/",a=`?${new URLSearchParams({key:"42137546-386b5be41212ccd429cab5a80",q:r,image_type:"photo",orientation:"horizontal",safeSearch:!0})}`,t=s+a;return fetch(t).then(e=>{if(!e.ok)throw new Error(`Network response was not ok, status: ${e.status}`);return e.json()}).catch(e=>{throw console.error(`${e}`),e})}function f({webformatURL:r,largeImageURL:s,tags:o,likes:a,views:t,comments:e,downloads:n}){return`
+  <a href="${s}" class="${l}">
+     <figure>
+      <img src="${r}" alt="${o}" class="gallery-image">
+      <figcaption class="gallery__figcaption">
+        <p class="image-item">Likes <span class="image-caption">${a}</span></p>
+        <p class="image-item">Views <span class="image-caption">${t}</span></p>
+        <p class="image-item">Comments <span class="image-caption">${e}</span></p>
+        <p class="image-item">Downloads <span class="image-caption">${n}</span></p>
+  </figcaption>
+  </figure>
+</a>`}function h(r){return r.map(f).join("")}function d(r){if(r.length===0){p.show({icon:"icon-close",messageSize:"16px",backgroundColor:"red",position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"});return}const s=h(r);c.innerHTML=s}
+//# sourceMappingURL=commonHelpers.js.map
